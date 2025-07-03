8c8bad3c213a2903d126ac0edba2812d94ad0719
michaelbel <michael-bel@outlook.com>
2025-07-03 20:46:18 +0300

    Update project

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.apache.commons.io.output.ByteArrayOutputStream
+import java.nio.charset.Charset
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+private val gitCommitsCount: Int by lazy {
+    when {
+        System.getProperty("os.name").contains("Windows", ignoreCase = true) -> 1
+        else -> {
+            val stdout = ByteArrayOutputStream()
+            exec {
+                commandLine("git", "rev-list", "--count", "HEAD")
+                standardOutput = stdout
+            }
+            stdout.toString(Charset.defaultCharset()).trim().toInt()
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmToolchain(libs.versions.jdk.get().toInt())
+    }
+}
+
+android {
+    namespace = "org.michaelbel.mergetag"
+    compileSdk = libs.versions.compile.sdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "org.michaelbel.mergetag"
+        minSdk = libs.versions.min.sdk.get().toInt()
+        targetSdk = libs.versions.target.sdk.get().toInt()
+        versionCode = gitCommitsCount
+        versionName = "1.0.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+base {
+    archivesName.set("MergeTag-v${android.defaultConfig.versionName}(${android.defaultConfig.versionCode})")
+}
+
+dependencies {
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.google.material)
+}
+
+tasks.register("printVersionName") { doLast { println(android.defaultConfig.versionName) } }
+tasks.register("printVersionCode") { doLast { println(android.defaultConfig.versionCode.toString()) } }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,14 +20,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "android-template" // todo Replace with your own app’s name
-include(
-    ":mobile",
-    ":wear",
-    ":tv",
-    ":auto",
-    ":xr",
-    ":core"
-)
+rootProject.name = "MergeTag"
+include(":app")
